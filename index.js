96e7a6dfa78cb1694e7dd301e53c58eb53b859fe
Nick Smith <smthnck@gmail.com>
2018-11-17 23:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,15 +61,15 @@ function checkCollision(rock) {
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
+  const rock = document.createElement('div');
 
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
+  var top = 0;
 
-  rock.style.top = top
+  rock.style.top = top;
 
   /**
    * Now that we have a rock, we'll need to append
